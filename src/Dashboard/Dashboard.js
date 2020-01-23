a75d72bdb1ed102bd2b561b9679233b7a73df3c4
Suryansh Tiwari <surayans.tiwari@ggktech.com>
2020-01-23 12:48:20 +0530

    added grid layout for dashboard

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,32 +1,188 @@
 import React, { Component } from 'react'
-import { Header, Icon, Image, Menu, Segment, Sidebar, Input, Container } from 'semantic-ui-react'
+import { Header, Icon, Image, Menu, Segment, Sidebar, Input, Container, Button, Card, Grid } from 'semantic-ui-react'
 import Login from '../Login/Login'
  
 export class Dashboard extends Component {
     render() {
         return (
-
-            <Sidebar
-                as={Menu}
-                animation='overlay'
-                icon='labeled'
-                vertical
-                visible
-                width='thin'
-                >
-                <Menu.Item as='a'>
-                    <Icon name='home' />
-                    Home
-                </Menu.Item>
-                <Menu.Item as='a'>
-                    <Icon name='gamepad' />
-                    Games
-                </Menu.Item>
-                <Menu.Item as='a'>
-                    <Icon name='camera' />
-                    Channels
-                </Menu.Item>
-                </Sidebar>
+            <Grid centered columns={3} padded='horizontally'>
+                <Grid.Column width={2}>
+                    <Sidebar
+                    as={Menu}
+                    animation='overlay'
+                    icon='labeled'
+                    vertical
+                    inverted
+                    visible
+                    width='thin'
+                    >
+                    <Menu.Item as='a'>
+                        <Icon name='home' />
+                        Home
+                    </Menu.Item>
+                    <Menu.Item as='a'>
+                        <Icon name='gamepad' />
+                        Games
+                    </Menu.Item>
+                    <Menu.Item as='a'>
+                        <Icon name='camera' />
+                        Channels
+                    </Menu.Item>
+                    </Sidebar>
+                </Grid.Column>
+                <Grid.Column width={12} textAlign='center' style={{paddingTop: '100px', paddingLeft: '220px'}}>
+                <Grid.Column width={4} >
+                    <Card.Group >
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                    </Card.Group>
+                </Grid.Column>
+                <Grid.Column width={4} >
+                    <Card.Group>
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                    </Card.Group>
+                </Grid.Column>
+                <Grid.Column width={4} >
+                    <Card.Group>
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                        <Card floated='right'> <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content></Card>
+                    </Card.Group>
+                </Grid.Column>
+                </Grid.Column>
+                <Grid.Column width={2}>
+                    <Button negative floated='right' size='large' style={{marginLeft: '60px', marginTop: '30px'}}>Logout</Button>
+                 </Grid.Column>
+            </Grid>
         )
     }
 }
